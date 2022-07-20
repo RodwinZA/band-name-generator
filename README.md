4508b036fabcf207ed72f597ad7f8501020b1b50
rodpiete021 <rodpiete021@student.wethinkcode.co.za>
2022-07-20 15:46:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 100 Days of Python - Day 1
+
+# Band Name Generator
+
+This band name generator takes two string inputs and concatenates it to suggest a name for your band.
---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+home_town = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+
+print("Your band name could be " + home_town + " " + pet_name)
